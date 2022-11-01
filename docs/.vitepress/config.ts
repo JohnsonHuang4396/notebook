0597ae1301a76610a80849ee4fb42103465b974f
黄启浩 <1771032483@qq.com>
2022-11-01 10:48:28 +0800

    [ init ] 初始化项目

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+export default {
+	title: "my notebook",
+	description: "第一个vitepress",
+	lang: "zh_CN",
+	themeConfig: {
+		siteTitle: "INeedAGodDamnGUN",
+		logo: "/imgs/gun.png",
+		socialLinks: [{ icon: "github", link: "https://github.com/Jhonson1z" }],
+	},
+};
