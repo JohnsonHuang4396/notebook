e4eb88cd4b98aaa39e7822d4410aacdce7c44a82
INeedAGodDamnGun <1771032483@qq.com>
2022-11-19 17:15:44 +0800

    [ update ] 增加我的笔记md

--- docs/.vitepress/basicMenuConfig.ts
+++ docs/.vitepress/basicMenuConfig.ts
@@ -1,14 +1,16 @@
-import { DefaultTheme } from "vitepress";
-import jsMenuModule from "./jsMenuConfig";
-import VueMenuModule from "./vueMenuConfig";
-import HTMLMenuConfig from "./HTMLMenuConfig";
-import mixMenuConfig from "./mixMenuConfig";
+import { DefaultTheme } from 'vitepress'
+import jsMenuModule from './jsMenuConfig'
+import VueMenuModule from './vueMenuConfig'
+import HTMLMenuConfig from './HTMLMenuConfig'
+import mixMenuConfig from './mixMenuConfig'
+import myNoteMenuConfig from './myNoteMenuConfig'
 
 const sidebar: DefaultTheme.Sidebar = [
-	...jsMenuModule,
-	...VueMenuModule,
-	...HTMLMenuConfig,
-	...mixMenuConfig,
-];
+  ...jsMenuModule,
+  ...VueMenuModule,
+  ...HTMLMenuConfig,
+  ...mixMenuConfig,
+  ...myNoteMenuConfig,
+]
 
-export default sidebar;
+export default sidebar
---
+++ docs/.vitepress/myNoteMenuConfig.ts
@@ -0,0 +1,10 @@
+const myNoteLink = '/my-note/md'
+
+export default [
+  {
+    text: 'MyNote',
+    collapsible: true,
+    collapsed: true,
+    items: [{ text: '面试总结', link: '/my-note/' }],
+  },
+]
