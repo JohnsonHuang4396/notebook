e6ccb76ddfc40e6ec9bc76ac6621dec2d1b69291
Johnson1z <83126532+JohnsonHuang4396@users.noreply.github.com>
2025-07-09 16:57:12 +0800

    更新文档导航配置，新增八股文和我的笔记链接，删除冗余的面试总结文档，优化项目结构以提升用户体验。同时，更新.vitepress缓存目录，新增多个依赖文件以支持新功能，确保项目构建效率和兼容性提升。

--- docs/.vitepress/myNoteMenuConfig.ts
+++ docs/.vitepress/myNoteMenuConfig.ts
@@ -5,13 +5,18 @@ export default [
     text: 'MyNote',
     collapsible: true,
     collapsed: true,
-    items: [{ text: '面试总结', link: '/my-review/' }]
+    items: [
+      { text: '面试总结', link: '/my-review/' },
+      { text: '八股文', link: `${myNoteLink}/boring-basic` },
+      { text: '我的笔记', link: `${myNoteLink}/my-note` },
+      { text: '面经好文', link: `${myNoteLink}/good-article` },
+    ]
   }
 ]
 
 export const myNoteNavConfig = [
   { text: '我的总结', link: '/my-review/' },
-  { text: '八股文', link: `${myNoteLink}/八股文` },
+  { text: '八股文', link: `${myNoteLink}/boring-basic` },
   { text: '我的笔记', link: `${myNoteLink}/my-note` },
   { text: '面经好文', link: `${myNoteLink}/good-article` },
 ]
